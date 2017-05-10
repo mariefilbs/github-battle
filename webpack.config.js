3e33b482848042d1714236eb6884ad1294d45396
mariefilbs <patriciamariefilbey@gmail.com>
2017-05-10 09:18:34 -0400

    Ran Webpack; Dist file created.

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports ={
   module: {
     rules:  [
       { test: /\.(js)$/, use: 'babel-loader' },
-      ( test: /\.css$/, use: [ 'style-loader', 'css-loader' ])
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
   plugins: [new HTMLWebpackPlugin({
